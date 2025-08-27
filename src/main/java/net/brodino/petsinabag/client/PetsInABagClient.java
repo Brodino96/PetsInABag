f8e1d4b43eeaae0aaff2c83bc0180292d52beed2
Brodino <brodino96@gmail.com>
2025-08-27 19:58:20 +0200

    debug command

--- src/main/java/net/brodino/petsinabag/client/PetsInABagClient.java
+++ src/main/java/net/brodino/petsinabag/client/PetsInABagClient.java
@@ -1,10 +1,29 @@
 package net.brodino.petsinabag.client;
 
+import net.brodino.petsinabag.client.screen.PetScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.minecraft.client.MinecraftClient;
 
 public class PetsInABagClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        this.registerCommand();
+    }
+
+    private void registerCommand() {
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(
+                    ClientCommandManager.literal("dio")
+                            .executes(context -> {
+                                MinecraftClient.getInstance().setScreen(new PetScreen());
+                                return 0;
+                            }
+                    )
+            );
+        });
+
     }
 }
